a2d7f29dc503ed63e7279aedc201e90ebcf46d42
Fertarius1108 <ferjesusurd@gmail.com>
2022-08-08 17:28:32 -0500

    css has been add to project

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const api_key='2daf9336-2f35-4df6-8c06-169d7172cf27';
-const api_url_random='https://api.thecatapi.com/v1/images/search?limit=2';
+const api_url_random='https://api.thecatapi.com/v1/images/search?limit=4';
 const api_url_favorites='https://api.thecatapi.com/v1/favourites';
 
 
@@ -8,22 +8,23 @@ async function loadRandomCats(){
     const data = await res.json();
 
     const toRender=[];
-    const section=document.getElementById("random cats");
+    const section=document.getElementById("random_cats");
+    section.innerHTML="";
     data.forEach(cat => {
         const figure=document.createElement('figure');
         const img=document.createElement('img');
         const btn=document.createElement('button');
-        const btnText=document.createTextNode("Delete cat from ur favorites");
+        const btnText=document.createTextNode("Add to favorites");
 
         img.src=cat.url;
         img.className="randomCat";
         btn.appendChild(btnText);
+        btn.className="btnAddFavorite";
         btn.onclick=()=>saveFavoriteCats(cat.id);
-        figure.appendChild(img,btn);
+        figure.append(img,btn);
         toRender.push(figure);
-        section.append(...toRender);
     });
-    toRender=[];
+    section.append(...toRender);
     console.log(data);
 }
 
@@ -37,22 +38,26 @@ async function loadFavoriteCats(){
 
     console.log(data);
 
+    const toRender=[];
+    const article = document.getElementById("artFavoriteCats");
+    article.innerHTML="";
     data.forEach(cat => {
-        const section = document.getElementById("favoriteKitties");
-        const article = document.createElement('article');
+        const figure = document.createElement('figure');
         const img = document.createElement('img');
         const btn = document.createElement('button');
-        const btnText = document.createTextNode("Delete cat from ur favorites");
+        const btnText = document.createTextNode("Delete");
 
         img.src=cat.image.url;
         img.className="favoriteCat";
         btn.appendChild(btnText);
-        article.appendChild(img);
-        article.appendChild(btn);
-        section.appendChild(article);
-    })
-
-
+        btn.className="btnDeleteFavorite";
+        btn.onclick=()=>deleteFavoriteCats(cat.id);
+        figure.className="figureDelete";
+        figure.append(img,btn);
+        toRender.push(figure);
+    });
+    article.append(...toRender);
+    console.log(data);
 }
 
 async function saveFavoriteCats(id){
@@ -66,10 +71,31 @@ async function saveFavoriteCats(id){
             image_id:id
         })
     })
-    const data = await res.json();
+    
+    if(res.status!==200){
+        console.log(res);
+    }else{
+        console.log("The cat has been saved in ur favorites");
+        loadFavoriteCats();
+    }
+}
 
-    console.log(res);
+async function deleteFavoriteCats(id){
+    console.log(id);
+    const res = await fetch(`${api_url_favorites}/${id}`,{
+        method:"DELETE",
+        headers:{
+            'x-api-key':api_key
+        }
+    })
+    const data = await res.json();
     console.log(data);
+    if(res.status!==200){
+        console.log(res);
+    }else{
+        console.log("The cat has been deleted");
+        loadFavoriteCats();
+    }
 }
 
     
